7d5babfc5661815ffc4961522a5515716e6fe80d
nliviu <nliviu@gmail.com>
2018-08-28 16:57:43 +0300

    clang-format

--- src/mgos_rpc_service_sd.c
+++ src/mgos_rpc_service_sd.c
@@ -5,16 +5,17 @@
 
 static void rpc_sd_open(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL != sd) {
-    //error
-    mg_rpc_send_errorf(ri, 400, "SD already open as %s!", mgos_sd_get_mount_point());
+    // error
+    mg_rpc_send_errorf(ri, 400, "SD already open as %s!",
+                       mgos_sd_get_mount_point());
     ri = NULL;
     return;
   }
   //"{sdmmc: %B, mount_point: %Q, format_if_mount_failed: %B}"
   bool sdmmc = false;
-  char* mount_point = NULL;
+  char *mount_point = NULL;
   bool format_if_mount_failed = true;
   json_scanf(args.p, args.len, ri->args_fmt, &sdmmc, &mount_point,
              &format_if_mount_failed);
@@ -25,8 +26,8 @@ static void rpc_sd_open(struct mg_rpc_request_info *ri, void *cb_arg,
 
   sd = mgos_sd_open(sdmmc, mount_point, format_if_mount_failed);
   if (NULL == sd) {
-    mg_rpc_send_errorf(ri, 400, "Failed to open SD (args: %.*s)",
-                       args.len, args.p);
+    mg_rpc_send_errorf(ri, 400, "Failed to open SD (args: %.*s)", args.len,
+                       args.p);
     return;
   }
   mg_rpc_send_responsef(ri, "SD created OK!");
@@ -37,9 +38,9 @@ static void rpc_sd_open(struct mg_rpc_request_info *ri, void *cb_arg,
 
 static void rpc_sd_close(struct mg_rpc_request_info *ri, void *cb_arg,
                          struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
     return;
   }
@@ -51,12 +52,12 @@ static void rpc_sd_close(struct mg_rpc_request_info *ri, void *cb_arg,
   (void) args;
 }
 
-static void rpc_sd_get_mount_point(struct mg_rpc_request_info *ri,
-                                   void *cb_arg, struct mg_rpc_frame_info *fi,
+static void rpc_sd_get_mount_point(struct mg_rpc_request_info *ri, void *cb_arg,
+                                   struct mg_rpc_frame_info *fi,
                                    struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
     return;
   }
@@ -64,10 +65,9 @@ static void rpc_sd_get_mount_point(struct mg_rpc_request_info *ri,
   struct json_out jsout = JSON_OUT_MBUF(&jsmb);
   mbuf_init(&jsmb, 0);
   json_printf(&jsout, "{mount_point: %Q}", mgos_sd_get_mount_point());
-
   mg_rpc_send_responsef(ri, "%.*s", jsmb.len, jsmb.buf);
-
   mbuf_free(&jsmb);
+
   (void) cb_arg;
   (void) fi;
   (void) args;
@@ -75,14 +75,14 @@ static void rpc_sd_get_mount_point(struct mg_rpc_request_info *ri,
 
 static void rpc_sd_list(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
     ri = NULL;
     return;
   }
-  //extract path
+  // extract path
   char *path = NULL;
   json_scanf(args.p, args.len, "{path: %Q}", &path);
 
@@ -96,21 +96,21 @@ static void rpc_sd_list(struct mg_rpc_request_info *ri, void *cb_arg,
   }
   mbuf_free(&jsmb);
   free(path);
+
   (void) cb_arg;
   (void) fi;
-  (void) args;
 }
 
 static void rpc_sd_mkdir(struct mg_rpc_request_info *ri, void *cb_arg,
                          struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
     ri = NULL;
     return;
   }
-  //extract path
+  // extract path
   char *path = NULL;
   json_scanf(args.p, args.len, ri->args_fmt, &path);
 
@@ -121,40 +121,35 @@ static void rpc_sd_mkdir(struct mg_rpc_request_info *ri, void *cb_arg,
   }
 
   char buf[256];
-  snprintf(buf, sizeof (buf), "%s/%s", mgos_sd_get_mount_point(), path);
+  snprintf(buf, sizeof(buf), "%s/%s", mgos_sd_get_mount_point(), path);
   int result = mkdir(buf, 0777);
   if (0 == result) {
-    //success
+    // success
     mg_rpc_send_responsef(ri, "{Created: %Q}", buf);
   } else {
-
     mg_rpc_send_errorf(ri, 400, "Could not create %s (errno=%d)", buf, errno);
   }
   free(path);
 
   (void) cb_arg;
   (void) fi;
-  (void) args;
 }
 
 static void rpc_sd_info(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
-
     return;
   }
 
   struct mbuf jsmb;
   struct json_out jsout = JSON_OUT_MBUF(&jsmb);
   mbuf_init(&jsmb, 0);
-
   mgos_sd_print_info(&jsout);
   LOG(LL_INFO, ("%.*s", jsmb.len, jsmb.buf));
   mg_rpc_send_responsef(ri, "%.*s", jsmb.len, jsmb.buf);
-
   mbuf_free(&jsmb);
 
   (void) cb_arg;
@@ -164,9 +159,9 @@ static void rpc_sd_info(struct mg_rpc_request_info *ri, void *cb_arg,
 
 static void rpc_sd_size(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
 
     return;
@@ -178,30 +173,37 @@ static void rpc_sd_size(struct mg_rpc_request_info *ri, void *cb_arg,
   json_printf(&jsout, "{sd_size: %llu}", size);
   mg_rpc_send_responsef(ri, "%.*s", jsmb.len, jsmb.buf);
   mbuf_free(&jsmb);
+
+  (void) cb_arg;
+  (void) fi;
+  (void) args;
 }
 
 static void rpc_sd_used(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
-    //error
+    // error
     mg_rpc_send_errorf(ri, 400, "No SD found!");
-
     return;
   }
+
   struct mbuf jsmb;
   struct json_out jsout = JSON_OUT_MBUF(&jsmb);
   mbuf_init(&jsmb, 0);
   uint64_t size = mgos_sd_get_fs_used(SD_FS_UNIT_BYTES);
   json_printf(&jsout, "{sd_used: %llu}", size);
   mg_rpc_send_responsef(ri, "%.*s", jsmb.len, jsmb.buf);
-
   mbuf_free(&jsmb);
+
+  (void) cb_arg;
+  (void) fi;
+  (void) args;
 }
 
 static void rpc_sd_free(struct mg_rpc_request_info *ri, void *cb_arg,
                         struct mg_rpc_frame_info *fi, struct mg_str args) {
-  struct mgos_sd* sd = mgos_sd_get_global();
+  struct mgos_sd *sd = mgos_sd_get_global();
   if (NULL == sd) {
     mg_rpc_send_errorf(ri, 400, "No SD found!");
     return;
@@ -213,8 +215,11 @@ static void rpc_sd_free(struct mg_rpc_request_info *ri, void *cb_arg,
   uint64_t size = mgos_sd_get_fs_free(SD_FS_UNIT_BYTES);
   json_printf(&jsout, "{sd_free: %llu}", size);
   mg_rpc_send_responsef(ri, "%.*s", jsmb.len, jsmb.buf);
-
   mbuf_free(&jsmb);
+
+  (void) cb_arg;
+  (void) fi;
+  (void) args;
 }
 
 bool mgos_rpc_service_sd_init() {
